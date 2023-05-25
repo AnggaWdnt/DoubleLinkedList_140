40bb828e0be04fbd9d879b43a9777665880c2408
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-05-25 15:27:23 +0700

    tambahan class doublelinkedlist

--- Doublelinkedlist_140/Doublelinkedlist_140.cpp
+++ Doublelinkedlist_140/Doublelinkedlist_140.cpp
@@ -2,10 +2,23 @@
 #include <string>
 using namespace std;
 
-class node {
+class Node {
 public:
 	int noMhs;
 	string name;
 	Node* next;
 	Node* prev;
+};
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+	DoubleLinkedList() {
+		void addNote();
+		bool search(int rollNo, Node * *previous, Node * *current);
+		bool deleteNode(int rollNo);
+		bool listEmpty();
+		void traverse();
+	}
 };
