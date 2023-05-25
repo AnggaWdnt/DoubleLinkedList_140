e0060cc95e91cad583cdd8c48347fb324fc3b5f5
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-05-25 20:22:30 +0700

    tambahan void DoubleLinkedList hapus

--- Doublelinkedlist_140/Doublelinkedlist_140.cpp
+++ Doublelinkedlist_140/Doublelinkedlist_140.cpp
@@ -114,3 +114,18 @@ void DoubleLinkedList::traverse() {
 		}
 	}
 }
+
+void DoubleLinkedList::hapus() {
+	if (listEmpty()) {
+		cout << "\nlist is empty" << endl;
+
+	}
+	cout << "\nEnter the roll number of the student whose record is be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number " << rollNo << " deleted " << endl;
+}
